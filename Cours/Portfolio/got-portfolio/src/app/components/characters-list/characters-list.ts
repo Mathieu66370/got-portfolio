cb4fd2ac964e937725baf3c95680fb293d570ed8
Mathieu <bosq.mathieu@gmail.com>
2025-12-10 12:02:02 +0100

    Second commit

---
+++ Cours/Portfolio/got-portfolio/src/app/components/characters-list/characters-list.ts
@@ -0,0 +1,35 @@
+import { Component } from '@angular/core';
+import {Characters} from '../../shared/models/characters.model';
+
+
+@Component({
+  selector: 'app-characters-list',
+  imports: [],
+  templateUrl: './characters-list.html',
+  styleUrl: './characters-list.scss',
+})
+export class CharactersList {
+  protected newCharacter: Characters = {
+    id: 1,
+    firstName: 'Mathieu',
+    lastName: 'TB',
+    fullName: 'Mathieu TB',
+    title: 'Roi',
+    family: '',
+    image: '',
+    imageUrl: ''
+  };
+
+
+protected charactersFromApi: Characters[] = [
+  {
+    id: 1,
+    firstName: 'Mathieu',
+    lastName: 'TB',
+    fullName: 'Mathieu TB',
+    title: 'Roi',
+    family: '',
+    image: '',
+    imageUrl: ''
+  }];
+}
